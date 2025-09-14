7f077d9d252cc2e30454d2b927db567cd8dd7aba
Saakshi Singh <Saakshi.Singh@truminds.com>
2025-09-14 21:51:53 +0530

    Ep-01

---
+++ app.js
@@ -0,0 +1,28 @@
+
+
+
+const parent = React.createElement(
+    "div",
+    {id:"parent"},
+    [
+        React.createElement(
+            "div",
+            {id:"child"},
+            [
+                React.createElement("h1",{id:"heading"},"I am the heading"),
+                React.createElement("h2",{},"I am h2")
+            ]
+        ),
+        React.createElement(
+            "div",
+            {id:"child2"},
+            [
+                React.createElement("h1",{id:"heading"},"I am the heading2"),
+                React.createElement("h2",{},"I am h2 tag from child2")
+            ]
+        )
+    ]
+);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent);
