79872829908b0737eedddac7d3901c18e13f495c
Saakshi Singh <Saakshi.Singh@truminds.com>
2025-09-25 14:59:54 +0530

    Ep-06

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,14 +1,64 @@
 import RestaurantCard from "./RestaurantCard";
 import restaurantsList from "../utils/mockData";
-import { useState } from "react";
+import Shimmer from "./Shimmer";
+import { useEffect, useState } from "react";
 const Body = () => {
   //creating a state variable
-  let [listOfRestaurants, setListOfRestuarants] = useState(restaurantsList);
+  let [listOfRestaurants, setListOfRestuarants] = useState([]);
+  let [searchText, setSearchText] = useState("");
+  let [filtered, setFiltered] = useState([]);
 
-  return (
+  useEffect(() => {
+    //fetch the data
+    fetchingData();
+  }, []);
+  const fetchingData = async () => {
+    //introducing some wait period to look at shimmer UI
+    await new Promise((resolve) => setTimeout(resolve, 1000)); // wait 2 seconds
+    const data = await fetch(
+      "https://raw.githubusercontent.com/namastedev/namaste-react/refs/heads/main/swiggy-api"
+    );
+
+    const json = await data.json();
+
+    //for re rendering the page with the fetched data , we use setter function
+    setListOfRestuarants(
+      json?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle?.restaurants
+    );
+    setFiltered(listOfRestaurants);
+  };
+
+  //if data is empty (still being fetched) -> screen should show loader
+  //conditional rendering using ternary operator
+  return listOfRestaurants.length === 0 ? (
+    <Shimmer />
+  ) : (
     <div className="body">
-      <div className="search">search bar here</div>
       <div className="filter">
+        <div className="search">
+          <input
+            type="text"
+            placeholder="Search here...."
+            className="search-box"
+            value={searchText}
+            onChange={(e) => {
+              setSearchText(e.target.value);
+            }}
+          />
+          <button
+            onClick={() => {
+              let filteredRestaurant = listOfRestaurants.filter((res) => {
+                return res.info.name
+                  .toLowerCase()
+                  .includes(searchText.toLowerCase());
+              });
+
+              setFiltered(filteredRestaurant);
+            }}
+          >
+            Search
+          </button>
+        </div>
         <button
           className="filter-btn "
           onClick={() => {
@@ -23,7 +73,7 @@ const Body = () => {
         </button>
       </div>
       <div className="res-container">
-        {listOfRestaurants.map((restaurant) => (
+        {filtered.map((restaurant) => (
           <RestaurantCard key={restaurant.info.id} resData={restaurant} />
         ))}
       </div>
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,8 @@
+import { useState } from "react";
 import { LOGO_URL } from "../utils/constants";
 
 const Header = () => {
+  let [btnText, setBtnText] = useState("Login");
   return (
     <div className="header">
       <div className="logo-container">
@@ -10,9 +12,18 @@ const Header = () => {
         <ul>
           <li>Home</li>
           <li>Offers</li>
-          <li>Sign In</li>
-
           <li>Cart</li>
+          <li>
+            <button
+              className="login"
+              onClick={() => {
+                let newbtnName = btnText === "Login" ? "Logout" : "Login";
+                setBtnText(newbtnName);
+              }}
+            >
+              {btnText}
+            </button>
+          </li>
         </ul>
       </div>
     </div>
